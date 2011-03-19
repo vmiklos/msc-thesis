76418907ccf11bb0edde424d75bea00e6b49b986
vmiklos <vmiklos@9676a468-5fa2-4bad-9590-2d94e5c9e11f>
2011-03-19 13:28:42 +0000

    proto-up: add html parser, upload now works

--- proto/proto-up.py
+++ proto/proto-up.py
@@ -4,6 +4,25 @@ import socket
 import urlparse
 import sys
 import time
+from sgmllib import SGMLParser
+
+def parsehtml(page):
+	class HTMLParser(SGMLParser):
+		def reset(self):
+			SGMLParser.reset(self)
+			self.lastmod = None
+			self.nextmod = False
+		def handle_data(self, text):
+			if text.strip() == "vti_timelastmodified":
+				self.nextmod = True
+			elif self.nextmod and not self.lastmod:
+				self.nextmod = False
+				self.lastmod = text.strip()
+	parser = HTMLParser()
+	parser.reset()
+	parser.feed(page)
+	parser.close()
+	return parser.lastmod
 
 headers = {
 	'Authorization': 'Basic YWRtaW46YWxmcmVzY28='
@@ -20,13 +39,15 @@ if response.status != 200:
 	raise Exception("failed to log in")
 print "ok, logged in"
 
-print "running getDocsMetaInfo..." # #6
+print "running getDocsMetaInfo..."
 body = "method=getDocsMetaInfo%3a12%2e0%2e0%2e6211&url%5flist=%5bhttp%3a%2f%2f127%2e0%2e0%2e1%3a7070%2falfresco%2fSPP%2fdocumentLibrary%2flocal%2edoc%3bhttp%3a%2f%2f127%2e0%2e0%2e1%3a7070%2falfresco%2fSPP%2fdocumentLibrary%5d&listHiddenDocs=false&listLinkInfo=false\n"
 conn = httplib.HTTPConnection(host, port)
 conn.request("POST", "/alfresco/SPP/_vti_bin/_vti_aut/author.dll", body, headers)
 response = conn.getresponse()
-print response.status, response.reason
-print response.read()
+html = response.read()
+if "failedUrls" in html:
+	raise Exception("failed to get meta info")
+lastmod = parsehtml(response.read()).split('|')[1]
 
 sock = open('local.doc')
 buf = sock.read()
@@ -35,7 +56,7 @@ sock.close()
 params = {
 	'method': 'put document:12.0.0.6211',
 	'service_name': '/alfresco/SPP',
-	'document': '[document_name=documentLibrary/local.doc;meta_info=[vti_timelastmodified;TW|%s]]' % time.strftime("%d %b %Y %H:%M:%S -0000"),
+	'document': '[document_name=documentLibrary/local.doc;meta_info=[vti_timelastmodified;TW|%s]]' % lastmod,
 	'put_option': 'edit',
 	'comment': '',
 	'keep_checked_out': 'false'
@@ -46,8 +67,7 @@ body = urllib.urlencode(params) + "\n" + buf
 headers['Content-Type'] = 'application/x-vermeer-urlencoded'
 headers['X-Vermeer-Content-Type'] = 'application/x-vermeer-urlencoded'
 conn = httplib.HTTPConnection(host, port)
-#conn.set_debuglevel(1)
 conn.request("POST", "/alfresco/SPP/_vti_bin/_vti_aut/author.dll", body, headers)
 response = conn.getresponse()
-print response.status, response.reason
-print response.read()
+if "successfully put document" not in response.read():
+	raise Exception("failed to put document")
