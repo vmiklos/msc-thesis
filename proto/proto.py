47b853db96137745ec655bd038d19c54942921c8
vmiklos <vmiklos@9676a468-5fa2-4bad-9590-2d94e5c9e11f>
2011-03-19 13:28:38 +0000

    move to oo

--- proto/proto.py
+++ proto/proto.py
@@ -4,102 +4,117 @@ import urlparse
 import sys
 from sgmllib import SGMLParser
 
-def ask(k, v):
-	line = None
-	try:
-		if sys.argv[1] == "-y":
-			line = v
-	except IndexError:
-		pass
-	if not line:
-		sys.stdout.write('%s [%s] ' % (k, v))
-		line = sys.stdin.readline().strip()
-	if not len(line):
-		line = v
-	return line
+class Handler:
+	def __init__(self):
+		# defaults
+		self.url = "http://127.0.0.1:7070/alfresco"
+		self.user = 'admin'
+		self.password = 'alfresco'
+		self.action = "open"
 
-def parsehtml(page):
-	class HTMLParser(SGMLParser):
-		def reset(self):
-			SGMLParser.reset(self)
-			self.items = {}
-		def start_tr(self, attrs):
-			fileattribute = None
-			id = None
-			for k, v in attrs:
-				if k == "fileattribute":
-					fileattribute = v
-				elif k == "id":
-					id = v
-			if fileattribute and id:
-				self.items[id] = fileattribute
-	parser = HTMLParser()
-	parser.reset()
-	parser.feed(page)
-	parser.close
-	return parser.items
+		self.url = self.ask('url', self.url)
+		self.user = self.ask('user', self.user)
+		self.password = self.ask('password', self.password)
 
-def handle_open(path):
-	# list folders
-	while True:
-		conn = httplib.HTTPConnection(host, port)
-		conn.request("GET", "%s/_vti_bin/owssvr.dll?location=&dialogview=FileOpen&FileDialogFilterValue=*.*" % path, headers = headers)
+		pr = urlparse.urlparse(self.url)
+		self.host, self.port = pr.netloc.split(':')
+		self.path = pr.path
+		self.headers = {'Authorization' : 'Basic ' + base64.encodestring('%s:%s' % (self.user, self.password))}
+
+		# log in
+		conn = httplib.HTTPConnection(self.host, self.port)
+		conn.request("GET", "/_vti_inf.html", headers = self.headers)
 		response = conn.getresponse()
 		if response.status != 200:
-			raise Exception("failed to read dir '%s/'" % path)
-		# extract the list of folders from the html response
-		html = response.read()
-		itemlist = parsehtml(html)
-		print "available items:"
-		names = sorted(itemlist.keys())
-		for n in names:
-			t = itemlist[n]
-			print "%s\t%s" % (n.split('/')[-1], t)
-		item = ask('item', names[0].split('/')[-1])
-		itemurl = "/".join(names[0].split('/')[:-1]) + "/" + item
-		path += "/" + item
-		if itemlist[itemurl] == "file":
-			break
-	print "ok, selected %s" % path
+			raise Exception("failed to log in")
+		print "ok, logged in"
+	
+	def handle(self):
+		while True:
+			print "possible actions: open, save, exit"
+			self.action = self.ask('action', self.action)
 
-	conn = httplib.HTTPConnection(host, port)
-	conn.request("GET", path, headers = headers)
-	response = conn.getresponse()
-	if response.status != 200:
-		raise Exception("failed to read file '%s'" % path)
+			if self.action == "open":
+				self.handle_open()
+			elif self.action == "save":
+				self.handle_save()
+			elif self.action == "exit":
+				break
 
-	localpath = path.split('/')[-1]
-	sock = open(localpath, "w")
-	sock.write(response.read())
-	sock.close()
-	print "saved to %s" % localpath
+	def ask(self, k, v):
+		line = None
+		try:
+			if sys.argv[1] == "-y":
+				line = v
+		except IndexError:
+			pass
+		if not line:
+			sys.stdout.write('%s [%s] ' % (k, v))
+			line = sys.stdin.readline().strip()
+		if not len(line):
+			line = v
+		return line
 
-# defaults
-url = "http://127.0.0.1:7070/alfresco"
-user = 'admin'
-password = 'alfresco'
+	def parsehtml(self, page):
+		class HTMLParser(SGMLParser):
+			def reset(self):
+				SGMLParser.reset(self)
+				self.items = {}
+			def start_tr(self, attrs):
+				fileattribute = None
+				id = None
+				for k, v in attrs:
+					if k == "fileattribute":
+						fileattribute = v
+					elif k == "id":
+						id = v
+				if fileattribute and id:
+					self.items[id] = fileattribute
+		parser = HTMLParser()
+		parser.reset()
+		parser.feed(page)
+		parser.close
+		return parser.items
 
-url = ask('url', url)
-user = ask('user', user)
-password = ask('password', password)
+	def handle_open(self):
+		path = self.path
+		# list folders
+		while True:
+			conn = httplib.HTTPConnection(self.host, self.port)
+			conn.request("GET", "%s/_vti_bin/owssvr.dll?location=&dialogview=FileOpen&FileDialogFilterValue=*.*" % path, headers = self.headers)
+			response = conn.getresponse()
+			if response.status != 200:
+				raise Exception("failed to read dir '%s/'" % path)
+			# extract the list of folders from the html response
+			html = response.read()
+			itemlist = self.parsehtml(html)
+			print "available items:"
+			names = sorted(itemlist.keys())
+			for n in names:
+				t = itemlist[n]
+				print "%s\t%s" % (n.split('/')[-1], t)
+			item = self.ask('item', names[0].split('/')[-1])
+			itemurl = "/".join(names[0].split('/')[:-1]) + "/" + item
+			path += "/" + item
+			if itemlist[itemurl] == "file":
+				break
+		print "ok, selected %s" % path
 
-pr = urlparse.urlparse(url)
-host, port = pr.netloc.split(':')
-path = pr.path
-headers = {'Authorization' : 'Basic ' + base64.encodestring('%s:%s' % (user, password))}
+		conn = httplib.HTTPConnection(self.host, self.port)
+		conn.request("GET", path, headers = self.headers)
+		response = conn.getresponse()
+		if response.status != 200:
+			raise Exception("failed to read file '%s'" % path)
 
-# log in
-conn = httplib.HTTPConnection(host, port)
-conn.request("GET", "/_vti_inf.html", headers = headers)
-response = conn.getresponse()
-if response.status != 200:
-	raise Exception("failed to log in")
-print "ok, logged in"
+		localpath = path.split('/')[-1]
+		sock = open(localpath, "w")
+		sock.write(response.read())
+		sock.close()
+		print "saved to %s" % localpath
 
-action = "open"
+	def handle_save(self):
+		pass
 
-print "possible actions: open, save"
-action = ask('action', action)
-
-if action == "open":
-	handle_open(path)
+if __name__ == "__main__":
+	h = Handler()
+	h.handle()
