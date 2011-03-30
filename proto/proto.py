274ce30ef05dce31ac706450d75d50f13566d8d7
vmiklos <vmiklos@9676a468-5fa2-4bad-9590-2d94e5c9e11f>
2011-03-30 09:59:03 +0000

    proto: default pass is the same for alfresco/sp

--- proto/proto.py
+++ proto/proto.py
@@ -22,11 +22,10 @@ class Handler:
 		if "--alfresco" in sys.argv:
 			self.url = "http://127.0.0.1:7070/alfresco"
 			self.user = 'admin'
-			self.password = 'alfresco'
 		else:
 			self.url = "http://vmiklos-sp:80"
 			self.user = r'vmiklos-sp\Administrator'
-			self.password = 'LaborImage'
+		self.password = 'alfresco'
 		self.action = "open"
 		self.openedurl = None
 		self.openedfile = None
