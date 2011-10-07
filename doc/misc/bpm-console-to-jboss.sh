dd703990c257f3adcb1fb7fa04bcc6e089a582ea
vmiklos <vmiklos@9676a468-5fa2-4bad-9590-2d94e5c9e11f>
2011-10-07 15:53:59 +0000

    bpm-console-to-jboss.sh: add jbpm-bam

--- doc/misc/bpm-console-to-jboss.sh
+++ doc/misc/bpm-console-to-jboss.sh
@@ -2,7 +2,8 @@
 
 cd $HOME/git/bpmc/bpm-console-gitsvn
 mvn install -DskipTests # produces $HOME/.m2/repository/org/jboss/bpm/gwt-console-server/2.1/gwt-console-server-2.1-jbpm.war
+cd $HOME/git/droolsjbpm/jbpm/jbpm-bam; mvn -DskipTests install # this has to be *before* -gwt!
 cd $HOME/git/droolsjbpm/jbpm/jbpm-gwt; mvn -DskipTests install # produces $HOME/.m2/repository/org/jbpm/jbpm-gwt-console/5.1.2-SNAPSHOT/jbpm-gwt-console-5.1.2-SNAPSHOT.war
 cd $HOME/git/droolsjbpm/jbpm/jbpm-distribution; mvn -DskipTests install # produces target/jbpm-5.1.2-SNAPSHOT-gwt-console.zip
-cp target/jbpm-5.1.2-SNAPSHOT-gwt-console.zip $HOME/git/droolsjbpm/jbpm/jbpm-installer/lib
+cp target/jbpm-5.1.2-SNAPSHOT-gwt-console.zip target/jbpm-5.1.2-SNAPSHOT-bin.zip $HOME/git/droolsjbpm/jbpm/jbpm-installer/lib
 cd $HOME/git/droolsjbpm/jbpm/jbpm-installer; ant install.jBPM-gwt-console.into.jboss
